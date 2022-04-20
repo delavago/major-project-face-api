f7648d6cf5aa132414573b8a3d90f5eb89702907
Delano Bailey <delano_bailey@hotmail.com>
2022-04-19 23:19:19 -0500

    updated face api

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask,request,redirect,url_for,send_from_directory
+from flask_cors import CORS
 import os
 import time
 import numpy
@@ -9,6 +10,7 @@ import face
 IMAGE_DIRECTORY = os.getcwd()
 
 app = Flask(__name__)
+CORS(app)
 app.config['UPLOAD_FOLDER'] = IMAGE_DIRECTORY
 
 @app.route('/')
@@ -24,10 +26,10 @@ def encode():
     image_data = face.load_file(file_path)
     encodings = face.generate_encodings(image_data)
     os.remove(file_path)
-    return {'encodings':encodings.tolist()}
+    return {'status': '200 OK','encodings':encodings.tolist()}
 
 @app.route('/compare', methods=['POST'])
 def compare_encodings():
     known = numpy.array(request.json['known_encodings'])
     incoming = numpy.array(request.json['incoming_encodings'])
-    return {'comparison_result':bool(face.compare_encodings(known, incoming))}
+    return {'status': '200 OK', 'comparison_result':bool(face.compare_encodings(known, incoming))}
--- face.py
+++ face.py
@@ -1,7 +1,9 @@
 import face_recognition
 
 def generate_encodings(image):
-    return face_recognition.face_encodings(image)[0]
+    encodings = face_recognition.face_encodings(image);
+    print(encodings)
+    return encodings[0]
 
 def compare_encodings(known_encodings,incoming_encodings) -> bool:
     return face_recognition.compare_faces([known_encodings],incoming_encodings,tolerance=0.4)[0]
